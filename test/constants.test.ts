377c89a2fb7de3d80b293f5bfbeb3ffd56799701
mooncakehero <71623368+mooncakehero@users.noreply.github.com>
2020-09-23 12:53:30 -0700

    Enable CI (#1)
    * Update CI.yml
    
    * Add lint:fix
    
    * Use @pancakeswap-libs/pancake-swap-core
    
    * Update CI.yml

--- test/constants.test.ts
+++ test/constants.test.ts
@@ -14,7 +14,7 @@ describe('constants', () => {
     // })
     //TODO: Replace with real PancakePair.json
     it('matches computed bytecode hash', () => {
-      expect(INIT_CODE_HASH).toEqual("0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66")
+      expect(INIT_CODE_HASH).toEqual('0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66')
     })
   })
 })
